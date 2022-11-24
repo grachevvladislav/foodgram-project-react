196518c25bcf2294f1471040781adda7738d239e
grachevvladislav <vladislav.grachev@gmail.com>
2022-11-24 17:20:02 +0300

    all users done!

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -35,7 +35,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'users.urls'
 
 TEMPLATES = [
     {
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
@@ -5,12 +6,13 @@ from .views import (UsersViewSet,  # create_user_view, token_view,
                     get_token_view, del_token_view, me_view, set_password)
 
 router = DefaultRouter()
-router.register('users', UsersViewSet, basename='users')
+router.register('', UsersViewSet, basename='users')
 
 urlpatterns = [
-    path('auth/token/login/', get_token_view, name='create_token'),
-    path('auth/token/logout/', del_token_view, name='del_token'),
-    path('users/me/', me_view, name='me'),
-    path('users/set_password/', set_password, name='set_password'),
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/auth/token/login/', get_token_view, name='create_token'),
+    path('api/auth/token/logout/', del_token_view, name='del_token'),
+    path('api/users/me/', me_view, name='me'),
+    path('api/users/set_password/', set_password, name='set_password'),
+    path('api/users', include(router.urls)),
 ]
