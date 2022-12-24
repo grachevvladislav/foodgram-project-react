ba9fac466341051669246cf6db8254e08fe2ce20
vlad <vladislav.grachev@gmail.com>
2022-12-24 22:27:27 +0300

    fix create tag and ingredient

--- backend/users/views.py
+++ backend/users/views.py
@@ -6,7 +6,6 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.exceptions import ValidationError
 from rest_framework.mixins import (CreateModelMixin, ListModelMixin,
                                    RetrieveModelMixin)
-from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
