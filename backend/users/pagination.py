a65cf7fe12f2e3a049b569db1cea07cf83b686bb
vlad <vladislav.grachev@gmail.com>
2022-12-24 22:28:52 +0300

    fix create tag and ingredient

--- backend/users/pagination.py
+++ backend/users/pagination.py
@@ -2,6 +2,5 @@ from rest_framework.pagination import LimitOffsetPagination
 
 
 class CustomPagination(LimitOffsetPagination):
-    #page_size_query_param = 'limit'
     limit_query_param = 'limit'
     offset_query_param = 'page'
